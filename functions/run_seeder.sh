7df0f4d541f5d32592a728584206ea4199acaec2
yashjeet <luthra.yashjeet@gmail.com>
2022-07-18 21:21:58 +0530

    finished dockerization of api

--- functions/run_seeder.sh
+++
@@ -1,8 +0,0 @@
-if [ -z "$1" ]
-  then
-    echo "Which env do you want to run the seeder?"
-    exit 1
-fi
-echo 'Running Seeder...'
-NODE_ENV=$1 npx env-cmd -f ./env/.env.$1 npx sequelize-cli db:seed:all --env=$1 
-
