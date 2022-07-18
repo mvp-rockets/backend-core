9f0a79c581b96a7474f421a76e96d22a3a9358e0
Hitesh <hitesh.bhati@napses.com>
2022-07-18 13:43:00 +0530

    test cases for user and updated pipelines.yml

--- functions/test/data/factory.js
+++ functions/test/data/factory.js
@@ -3,9 +3,7 @@ const { factory } = require('factory-girl');
 const loadFactory = async () => {
     factory.define('user', Object, {
         id: factory.chance('guid'),
-        mobile: factory.chance('phone', { mobile: true }),
-        countryCode: factory.chance('integer', { min: 1, max: 1000 }),
-        timezone: factory.chance('city')
+        name: factory.chance('name')
     });
 };
 
--- functions/test/helpers/data.js
+++ functions/test/helpers/data.js
@@ -1,6 +1,6 @@
 const DeleteEntityById = require('test/data/delete-entity-by-id');
 const definations = require('test/data/factory').factory;
-
+const AddUserQuery = require('resources/users/queries/add-user-query');
 
 const entity = async (name, replace) => new Promise(async (resolve, reject) => {
     let data = await definations.build(name);
@@ -12,7 +12,14 @@ const entity = async (name, replace) => new Promise(async (resolve, reject) => {
 
 const buildEntity = (name) => entity(name);
 
+const user = {
+    name: 'user',
+    create: (user) => [new AddUserQuery({ id: user.id, name: user.name })],
+    build: () => entity('user'),
+    delete: (user) => [new DeleteEntityById(user.id, 'User')]
+};
 
 module.exports = {
-    buildEntity
+    buildEntity,
+    user
 };
