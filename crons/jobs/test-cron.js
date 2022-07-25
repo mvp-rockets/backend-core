cd58176464e7232c0422c220941c9655b37363d2
yashjeet <luthra.yashjeet@gmail.com>
2022-07-25 13:10:51 +0530

    testing throw secnario

--- crons/jobs/test-cron.js
+++ crons/jobs/test-cron.js
@@ -6,3 +6,7 @@ cronitor.wraps(cron);
 cronitor.schedule('SendWelcomeEmail', '*/2 * * * *', function() {
     console.log('Sending welcome email to new sign ups every five minutes.');
 });
+
+// cronitor.schedule('SendWelcomeEmail', '*/2 * * * *', function() {
+//     console.log('Sending welcome email to new sign ups every five minutes.');
+// });
