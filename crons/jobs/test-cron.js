79a5e1ed4558137a8e1a5a899e887bf0360bf524
yashjeet <luthra.yashjeet@gmail.com>
2022-07-26 10:21:06 +0530

    minor fixs

--- crons/jobs/test-cron.js
+++ crons/jobs/test-cron.js
@@ -6,7 +6,3 @@ cronitor.wraps(cron);
 cronitor.schedule('SendWelcomeEmail', '*/2 * * * *', function() {
     console.log('Sending welcome email to new sign ups every five minutes.');
 });
-
-// cronitor.schedule('SendWelcomeEmail', '*/2 * * * *', function() {
-//     console.log('Sending welcome email to new sign ups every five minutes.');
-// });
