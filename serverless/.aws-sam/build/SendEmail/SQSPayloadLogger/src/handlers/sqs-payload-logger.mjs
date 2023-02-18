3805ef6fed5de7cac75252427360be4d371abffc
yashjeet <luthra.yashjeet@gmail.com>
2023-02-18 12:51:30 +0530

    added common layer between lambda

--- serverless/.aws-sam/build/SendEmail/SQSPayloadLogger/src/handlers/sqs-payload-logger.mjs
+++ serverless/.aws-sam/build/SendEmail/SQSPayloadLogger/src/handlers/sqs-payload-logger.mjs
@@ -1,9 +1,11 @@
 /**
  * A Lambda function that logs the payload received from SQS.
  */
+import axios from 'axios';
 export const sqsPayloadLoggerHandler = async (event, context) => {
     // All log statements are written to CloudWatch by default. For more information, see
     // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
+    console.log("axios", axios)
     console.log("I am in send email")
     console.info(JSON.stringify(event));
 }
