c7fdbc07d38f25dd78855998f2037149a488c099
yashjeet <luthra.yashjeet@gmail.com>
2023-02-18 20:32:56 +0530

    importing function from common layer

--- serverless/.aws-sam/build/SendSms/SQSPayloadLogger/src/handlers/sqs-payload-logger.mjs
+++ serverless/.aws-sam/build/SendSms/SQSPayloadLogger/src/handlers/sqs-payload-logger.mjs
@@ -2,10 +2,12 @@
  * A Lambda function that logs the payload received from SQS.
  */
 import axios from 'axios';
+import helloWorld from '/opt/commonLayer';
 export const sqsPayloadLoggerHandler = async (event, context) => {
     // All log statements are written to CloudWatch by default. For more information, see
     // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
     console.log("I am in send sms")
+    console.log("Is hello world", helloWorld());
     console.log("axios", axios)
     console.info(JSON.stringify(event));
 }
--- serverless/send-sms/src/handlers/sqs-payload-logger.mjs
+++ serverless/send-sms/src/handlers/sqs-payload-logger.mjs
@@ -2,10 +2,12 @@
  * A Lambda function that logs the payload received from SQS.
  */
 import axios from 'axios';
+import { helloWorld }from '/opt/nodejs/index.js';
 export const sqsPayloadLoggerHandler = async (event, context) => {
     // All log statements are written to CloudWatch by default. For more information, see
     // https://docs.aws.amazon.com/lambda/latest/dg/nodejs-prog-model-logging.html
     console.log("I am in send sms")
     console.log("axios", axios)
+    console.log("Is hello world", helloWorld());
     console.info(JSON.stringify(event));
 }
