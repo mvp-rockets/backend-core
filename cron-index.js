b0487e4ffc437b791b43aaaf3ee7a6853ab23da6
ram <rkbhambri09@gmail.com>
2023-05-04 15:16:36 +0530

    remove dotenv and export secret variables to process env

--- cron-index.js
+++ cron-index.js
@@ -1,21 +1,18 @@
-require('app-module-path').addPath(__dirname);
+require("app-module-path").addPath(__dirname);
 
-const dotenv = require('dotenv');
+const { logError } = require("lib");
 
-dotenv.config({ path: `./env/.env.${process.env.NODE_ENV}` });
-const { logError } = require('lib')
+require("crons/heartbeat");
+require("crons/jobs/test-cron");
 
-require('crons/heartbeat')
-require('crons/jobs/test-cron')
-
-process.on('unhandledRejection', (error) => {
-    console.log('unhandledRejection');
-    logError('SQS index unhandledRejection', { error });
+process.on("unhandledRejection", (error) => {
+    console.log("unhandledRejection");
+    logError("SQS index unhandledRejection", { error });
     process.exit(1);
 });
 
-process.on('uncaughtException', (error) => {
-    console.log('uncaughtException');
-    logError('SQS index uncaughtException', { error });
+process.on("uncaughtException", (error) => {
+    console.log("uncaughtException");
+    logError("SQS index uncaughtException", { error });
     process.exit(1);
 });
--- index.js
+++ index.js
@@ -1,17 +1,14 @@
-require('app-module-path').addPath(__dirname);
+require("app-module-path").addPath(__dirname);
 
-const express = require('express');
+const express = require("express");
 
-const cors = require('cors');
-const dotenv = require('dotenv');
-
-dotenv.config({ path: `./env/.env.${process.env.NODE_ENV}` });
-const { Logger } = require('@mvp-rockets/namma-lib');
-const config = require('config/config');
+const cors = require("cors");
+const { Logger } = require("@mvp-rockets/namma-lib");
+const config = require("config/config");
 
 Logger.initialize({
     isEnable: config.awsCloudwatch.enableAwsLogger,
-    type: 'aws',
+    type: "aws",
     environment: config.env,
     clsNameSpace: config.clsNameSpace,
     configurations: {
@@ -19,24 +16,24 @@ Logger.initialize({
         accessKeyId: config.awsCloudwatch.accessKeyId,
         secretKey: config.awsCloudwatch.secretKey,
         logGroupName: config.awsCloudwatch.logGroupName,
-        logStreamName: config.awsCloudwatch.logStreamName
-    }
+        logStreamName: config.awsCloudwatch.logStreamName,
+    },
 });
-const cls = require('cls-hooked');
+const cls = require("cls-hooked");
 
-const bodyParser = require('body-parser');
-const { token } = require('@mvp-rockets/namma-lib');
+const bodyParser = require("body-parser");
+const { token } = require("@mvp-rockets/namma-lib");
 
 token.initialize(config.jwtSecretKey);
 
-const { ApiError } = require('lib');
-const { logError, logInfo } = require('lib');
-const { HTTP_CONSTANT } = require('@mvp-rockets/namma-lib');
+const { ApiError } = require("lib");
+const { logError, logInfo } = require("lib");
+const { HTTP_CONSTANT } = require("@mvp-rockets/namma-lib");
 
 const app = express();
-const server = require('http').createServer(app);
-const Route = require('route');
-const uuid = require('uuid');
+const server = require("http").createServer(app);
+const Route = require("route");
+const uuid = require("uuid");
 
 Route.setApp(app);
 
@@ -49,66 +46,68 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
     const namespace = cls.getNamespace(config.clsNameSpace);
-    const platform = req.headers['x-platform'] || 'unknown-platform';
+    const platform = req.headers["x-platform"] || "unknown-platform";
     namespace.run(() => {
-        namespace.set('traceId', uuid.v4());
+        namespace.set("traceId", uuid.v4());
         logInfo(`${req.method} ${req.originalUrl}`, { ...req.body, platform });
         next();
     });
 });
 
 // HealthCheck endpoints
-const healthCheckApi = require('resources/health-check-api');
-const healthCheckDbAPi = require('resources/health-check-db-api');
+const healthCheckApi = require("resources/health-check-api");
+const healthCheckDbAPi = require("resources/health-check-db-api");
 
-app.get('/health-check-api', healthCheckApi);
-app.get('/health-check-db', healthCheckDbAPi);
+app.get("/health-check-api", healthCheckApi);
+app.get("/health-check-db", healthCheckDbAPi);
 
-require('./api-routes');
+require("./api-routes");
 
 app.use((req, res, next) => {
-    const err = new ApiError('Not Found', 'Resource Not Found!', HTTP_CONSTANT.NOT_FOUND);
+    const err = new ApiError("Not Found", "Resource Not Found!", HTTP_CONSTANT.NOT_FOUND);
     next(err);
 });
 
 app.use((error, request, response, next) => {
-    const platform = request.headers['x-platform'] || 'unknown-platform';
+    const platform = request.headers["x-platform"] || "unknown-platform";
     if (error.constructor === ApiError) {
-        logError('Failed to execute the operation', {
+        logError("Failed to execute the operation", {
             value: error.error,
             stack: error.error ? error.error.stack : [],
-            platform
+            platform,
         });
-        if (error.code) { response.status(error.code); }
+        if (error.code) {
+            response.status(error.code);
+        }
         response.send({
             status: false,
-            message: error.errorMessage
+            message: error.errorMessage,
         });
     } else {
         response.status(501);
-        logError('Failed to execute the operation', { value: error, stack: error.stack, platform });
+        logError("Failed to execute the operation", { value: error, stack: error.stack, platform });
         response.send({
             status: false,
-            errorType: 'unhandled',
-            message: 'Something went wrong!'
+            errorType: "unhandled",
+            message: "Something went wrong!",
         });
     }
 });
 
-process.on('unhandledRejection', (error) => {
+process.on("unhandledRejection", (error) => {
     console.log(error);
-    logError('unhandledRejection', { error });
+    logError("unhandledRejection", { error });
 });
 
-process.on('uncaughtException', (error) => {
+process.on("uncaughtException", (error) => {
     console.log(error);
-    logError('uncaughtException', { error });
+    logError("uncaughtException", { error });
 });
 
-process.on('SIGTERM', () => {
-    logInfo('SIGTERM signal received: closing HTTP server');
+process.on("SIGTERM", () => {
+    logInfo("SIGTERM signal received: closing HTTP server");
     server.close(() => {
-        logInfo('HTTP server closed');
+        logInfo("HTTP server closed");
     });
 });
 
