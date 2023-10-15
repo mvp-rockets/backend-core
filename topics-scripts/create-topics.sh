7ae9d0a2b850c6fc19600f16ee22bdeeaefbd606
Vishal Jayapalan <vishal.jayapalan@napses.com>
2023-10-15 21:12:42 +0530

    fixed the create topics scripts google_application_credentials

--- topics-scripts/create-topics.sh
+++ topics-scripts/create-topics.sh
@@ -3,6 +3,8 @@ if [ -z "$1" ]
     echo "Which env do you create topics?"
     exit 1
 fi
-# cd afto_api
+
 echo 'Running create topics...'
+export $(grep GOOGLE_APPLICATION_CREDENTIALS "../env/.env.$1")
+export GOOGLE_APPLICATION_CREDENTIALS="../$GOOGLE_APPLICATION_CREDENTIALS"
 NODE_ENV=$1 node ./create-topics.js
