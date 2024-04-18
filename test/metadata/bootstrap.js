1bbbf5956e994ed87a04ac8073b698b4f63e454d
Satish Bhat <satish@napses.com>
2024-04-18 12:38:16 +0530

    Migrated test case env from NODE_ENV to APP_ENV

--- test/metadata/bootstrap.js
+++ test/metadata/bootstrap.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: `./env/.env.${process.env.NODE_ENV}` });
+dotenv.config({ path: `./env/.env.${process.env.APP_ENV}` });
 require('app-module-path').addPath(`${path.resolve()}/test`);
 require('app-module-path').addPath(`${path.resolve()}/`);
 require('../../api-routes');
